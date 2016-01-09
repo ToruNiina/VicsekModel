73b85d99ef0041af31af7339fc46805a813de9f5
ToruNiina <niina.toru.68u@gmail.com>
2016-01-09 17:16:57 +0900

    change: system default parameter

--- scripts/make_gif.gp
+++ scripts/make_gif.gp
@@ -5,6 +5,6 @@ set yrange[-10:110]
 set key off
 set size ratio 1.0
 
-do for [i=0:100:1]{
-    plot 'vicsek.dat' index i using 2:3:4:5 with vector
+do for [i=0:200:1]{
+    plot 'vicsek.dat' index i*5 using 2:3:4:5 with vector
 }
--- src/System.hpp
+++ src/System.hpp
@@ -35,15 +35,15 @@ namespace vicsek
         private:
 
             int seed = 123;
-            uInt maxstep = 100;
-            uInt nlist_renew_step = 1;
+            uInt maxstep = 1000;
+            uInt nlist_renew_step = 10;
             uInt observe_step = 1;
-            uInt particle_number = 1000;
-            double box_width = 5e1;
-            double box_height = 5e1;
+            uInt particle_number = 5000;
+            double box_width = 1e2;
+            double box_height = 1e2;
             double particle_velocity = 1e0;
             double cutoff_distance = 2.5;
-            double noise_intensity = 1e-2 * M_PI;
+            double noise_intensity = 0.5 * M_PI;
             std::string output_filename = std::string("vicsek.dat");
             RegionSptr region;
             TimeStepperSptr time_stepper;
